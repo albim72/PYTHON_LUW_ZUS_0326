8490832ebff12a23f6b27ff927b04a578470d9dc
albim72 <marcin.albiniak72@gmail.com>
2026-03-02 14:02:57 +0100

    Add functions for coaching and price transformation

--- DZIEN_1/python_funkcyjnie/python_funkcyjnie.py
+++ DZIEN_1/python_funkcyjnie/python_funkcyjnie.py
@@ -31,3 +31,33 @@ def evaluate(time, scoring_strategy):
 
 print(evaluate(100, score_by_time))
 print(evaluate(300, score_by_consistancy))
+
+#ex3 - funkcja jako fabryka
+#generator osobowości
+
+def make_coach(multiplier):
+    def coach(speed):
+        return speed * multiplier
+    return coach
+
+strict_coach = make_coach(1.3)
+calm_coach = make_coach(0.96)
+
+print(strict_coach(10))
+print(calm_coach(10))
+
+#ex4 - kompozycja
+#pipeline transformacji
+
+def add_tax(x):
+    return x * 1.23
+
+def apply_discount(x):
+    return x * 0.9
+
+def compose(f, g):
+    return lambda x: f(g(x))
+
+final_price = compose(add_tax, apply_discount)
+
+print(final_price(100)) 
