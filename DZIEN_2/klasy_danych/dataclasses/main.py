ed233716d2391391fafff26161408e3f82ae24c5
albim72 <marcin.albiniak72@gmail.com>
2026-03-03 15:18:49 +0100

    Update main.py

--- DZIEN_2/klasy_danych/dataclasses/main.py
+++ DZIEN_2/klasy_danych/dataclasses/main.py
@@ -1,5 +1,6 @@
 from dt_ext1 import User
 from dt_ext2 import User
+from dt_ext3 import ShoppingCart
 
 def demo_1_user() -> None:
     print("\n[1] User")
@@ -13,10 +14,20 @@ def demo_1a_user() -> None:
     print(u)
     print(u.contact_card())
 
+def demo_2_cart() -> None:
+    print("\n[2] ShoppingCart")
+    cart = ShoppingCart("marcin")
+    cart.add("coffee")
+    cart.add("protein_bar")
+    cart.add_discount("SPRING10", 10.0)
+    print(cart.summary())
+    print("items:", cart.items)
+    print("discounts:", cart.discounts)
+
 
 if __name__ == "__main__":
     demo_1_user()
     demo_1a_user()
-    # demo_2_cart()
+    demo_2_cart()
     # demo_3_money()
     # demo_4_runsession()
