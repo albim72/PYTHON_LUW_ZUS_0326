78a95c4247e0791c6882051e4f773d4fe900e29e
albim72 <marcin.albiniak72@gmail.com>
2026-06-22 13:44:43 +0200

    Create zadanie_kolekcje.py

---
+++ DZIEN_1/zadanie_kolekcje.py
@@ -0,0 +1,26 @@
+Zadanie: Analiza sprzedaży produktów
+
+Masz dane o sprzedaży. Każda sprzedaż jest zapisana jako krotka:
+
+("produkt", liczba_sztuk)
+
+Wszystkie sprzedaże są zapisane w liście.
+
+sales = [
+    ("herbata", 3),
+    ("kawa", 2),
+    ("herbata", 1),
+    ("cukier", 5),
+    ("kawa", 4),
+    ("miód", 2),
+    ("herbata", 2)
+]
+Polecenie
+
+Napisz program, który:
+
+wypisze wszystkie sprzedaże,
+utworzy zbiór unikalnych produktów,
+policzy, ile różnych produktów sprzedano,
+utworzy słownik, w którym kluczem jest produkt, a wartością łączna liczba sprzedanych sztuk,
+wypisze końcowe podsumowanie sprzedaży.
